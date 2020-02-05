fc71b25fafef3afb97e03365f138eae77884e908
charles <codelyfewithcharlie@gmail.com>
2020-02-04 21:05:33 -0500

    update add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = add(1, 2);
-
 let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
 
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + Number(userInput.value);
+  outputResult(currentResult, calculationDescription);
 }
